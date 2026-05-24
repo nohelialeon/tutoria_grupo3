b7837a4c07912d7a441506faa5099310b9af857f
nohelialeon <nleon9351@utm.edu.ec>
2026-05-24 13:57:42 -0500

    Delete tutoriasyasesorias2/tutoriasyasesorias directory

--- tutoriasyasesorias2/tutoriasyasesorias/js/models/entity.js
+++
@@ -1,9 +0,0 @@
-export class TutoriaEntity {
-    constructor(docente, materia, fecha) {
-        this.id = Date.now(); // ID único autogenerado
-        this.docente = docente.trim();
-        this.materia = materia;
-        this.fecha = fecha;
-        this.estado = "Pendiente"; // Regla de Negocio: Estado inicial automático
-    }
-}
